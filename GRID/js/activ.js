fdffb60f3ddcb6e2b71c2627b2963d92d64b3d7e
RookwoodTV <rookwood@outlook.fr>
2023-06-28 16:56:00 +0200

    o

---
+++ GRID/js/activ.js
@@ -0,0 +1,6 @@
+// Navigation avec les liens actifs sur le page en cours
+
+// 1 - Ajouter l'IS #activ_nav suur la balise <nav>
+// 2 - Vérifier que la classe .activ est bien dans le fichier CSS
+// 3 - Créer la fonction setAtiv() pour ajouter la classe .activ sur le lien URL en cours
+
